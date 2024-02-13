249f9730823a3117c97286e1227e142e82f52191
shartlesville <159401069+shartlesville@users.noreply.github.com>
2024-02-12 21:05:32 -0700

    Update pick_a_number_game.py

--- pick_a_number_game.py
+++ pick_a_number_game.py
@@ -1,22 +1,21 @@
 from random import randint
 
-name = input("Tell me your name: ")
-print(f'Hi {name}, shall we play a game? Please enter yes or no. ') #add a y or n option
-if input() == "yes":
-    print(f"Great! I'm thinking of a number between 1 and 100. You have 8 tries to guess it. What number do you think it is?")
-else:
-    print(f"Ok, {name}, maybe next time.")
-
+name = input("Tell me your name: "'\n')
+print(f'Hi {name}, shall we play a game?')
+print()
+print(f"I'm thinking of a number between 1 and 100."'\n'"You have 8 chances to guess it. What number do you think it is?"'\n')
 
 secret_number = randint(1,100)
 guesses = 8
-for guess in range(9):
+for guess in range(1,9):
     guess = int(input("What is your guess: "))
     if guess > secret_number:
-       print(f"Too high, try again. You have {guesses - 1} more chances.")
+       print(f"Too high. You have {guesses - 1} more chances.")
     elif guess < secret_number:
-       print(f"Too low, try again. You have {guesses - 1} more chances.")
+       print(f"Too low. You have {guesses - 1} more chances.")
     elif guess == secret_number:
        print(f"Congratulations! You win! It took you {guesses} tries!")
        break
     guesses = guesses - 1
+if guesses == 0:
+    print(f"Sorry, {name}, you have run out of tries. The secret number was {secret_number}. Better luck next time.")
