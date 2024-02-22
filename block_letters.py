cf33e13ddff20705c745841bd794b85012ba70da
shartlesville <159401069+shartlesville@users.noreply.github.com>
2024-02-21 22:35:03 -0700

    Add files via upload

---
+++ block_letters.py
@@ -0,0 +1,9 @@
+#Krisann Robles
+#I like to sing
+print('K   K  RRRR')
+print('K  K   R   R')
+print('K K    R   R')
+print('K      RRRR')
+print('K K    R R')
+print('K  K   R  R')
+print('K   K  R   R')
